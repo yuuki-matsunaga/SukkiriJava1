7906a0cbb098e1cd9ced6cbc64dde0371ed2c7d4
yuuki-matsunaga <matsunaga.ymk@gmail.com>
2022-01-04 17:53:18 +0900

    Finish Chapter1

---
+++ Chapter1/Question1.java
@@ -0,0 +1,12 @@
+package Chapter1;
+
+public class Question1 {
+  public static void main(String args[]) {
+    int a = 3;
+    int b = 5;
+    int c = a * b;
+
+    System.out.println("縦幅3 横幅5の長方形の面積は、" + c);
+
+  }
+}
