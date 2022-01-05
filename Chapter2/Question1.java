e78afee3b695ab21e50e8692722efc9830e4cd1f
yuuki-matsunaga <matsunaga.ymk@gmail.com>
2022-01-05 19:20:13 +0900

    finish Chapter2

---
+++ Chapter2/Question1.java
@@ -0,0 +1,10 @@
+package Chapter2;
+
+public class Question1{
+  public static void main(String args[]){
+    int x = 5;
+    int y = 10;
+    String ans = "x + yは" + ( x + y );
+    System.out.println(ans);
+  }
+}
